8f49247ae7e7f8e7bab532c57778e7f52c0b61d2
Paul Lernmark <paul@lernmark.net>
2014-10-03 13:48:09 +0200

    zzz

--- airup_api.py
+++ airup_api.py
@@ -7,15 +7,33 @@ as well as those methods defined in an API.
 
 import logging
 import endpoints
+import urllib
 from google.appengine.ext import db
 from google.appengine.api import taskqueue
+from google.appengine.api import urlfetch
 from protorpc import messages
 from protorpc import message_types
 from protorpc import remote
 import datetime
 import time
 import json
+import random
 package = 'Airup'
+
+
+
+class ZoneDetailPersist(db.Model):
+    """Models an individual Record entry with content and date."""
+    #timestamp = db.FloatProperty()
+    title = db.StringProperty()
+    subtitle = db.StringProperty()
+    max24Hr = db.FloatProperty()
+    min24Hr = db.FloatProperty()
+    co = db.FloatProperty()
+    no2 = db.FloatProperty()
+    index = db.FloatProperty()
+    #id = db.FloatProperty()
+
 
 class Record(messages.Message):
     """Record that stores a message."""
@@ -37,9 +55,8 @@ class TodayType(messages.Message):
     best = messages.MessageField(TodayDetail, 1, repeated=False)
     worst = messages.MessageField(TodayDetail, 2, repeated=False)
 
-#required=True,variant=messages.Variant.INT32
 class ZoneDetail(messages.Message):
-    hood = messages.StringField(1)
+    title = messages.StringField(1)
     index = messages.FloatField(2)
     co = messages.FloatField(3)
     no2 = messages.FloatField(4)
@@ -47,7 +64,8 @@ class ZoneDetail(messages.Message):
     min24Hr = messages.FloatField(6)
     timestamp = messages.FloatField(7)
     id = messages.FloatField(8)
-    hoodDescription = messages.StringField(9)
+    subtitle = messages.StringField(9)
+
 
 class ZoneMessage(messages.Message):
     key = messages.StringField(1)
@@ -56,7 +74,7 @@ class ZoneMessage(messages.Message):
     timestamp = messages.FloatField(4)
 
 
-    #{"hood": "Hornstull", "min24Hr": "111", "timestamp": "1404916639.81", "max24Hr": "333", "no2": "33", "index": "222", "co": "22"}
+    #{"title": "Hornstull", "min24Hr": "111", "timestamp": "1404916639.81", "max24Hr": "333", "no2": "33", "index": "222", "co": "22"}
 
 class RecordCollection(messages.Message):
     """Collection of Records."""
@@ -98,6 +116,54 @@ class JsonProperty(db.TextProperty):
 class Report(db.Model):
     name = db.StringProperty()
     report = JsonProperty()    
+
+
+tableAqiIndex = [ range(0, 50, 1),range(51, 100, 1),range(101, 150, 1),range(151, 200, 1),range(201, 300, 1),range(301, 400, 1),range(401, 500, 1) ]
+tableCo = [ range(0, 44, 1),range(45, 94, 1),range(95, 124, 1),range(125, 154, 1),range(155, 304, 1),range(305, 404, 1),range(405, 504, 1) ] 
+tableNo2 = [ range(0, 53, 1),range(54, 100, 1),range(101, 360, 1),range(361, 640, 1),range(650, 1240, 1),range(1250, 1640, 1),range(1650, 2040, 1) ] 
+tablePm10 = [ range(0, 54, 1),range(55, 154, 1),range(155, 254, 1),range(255, 354, 1),range(355, 424, 1),range(425, 504, 1),range(505, 604, 1) ] 
+
+def index(table, v, fac):
+    row = [i for i,l in enumerate(table) if int(v*fac) in l][0]
+    bpLow = float(table[row][0])/fac
+    bpHigh = (float(table[row][len(table[row])-1]+1)/fac)
+    iLow = tableAqiIndex[row][0]
+    iHigh = tableAqiIndex[row][len(tableAqiIndex[row])-1]+1
+    index = (
+        (float(iHigh) - float(iLow)) / 
+        (float(bpHigh) - float(bpLow))
+        ) * (float(v)-(float(bpLow))) + float(iLow)
+    return int(index)
+
+
+def aqi(values):
+    print "---------------------------------"
+    co=values["co"]
+    pm10=values["pm10"]
+    no2=values["no2"]
+
+    f = 0
+    coIndex = 0
+    pm10Index = 0
+    no2Index = 0
+
+    if co:
+        coIndex = index(tableCo, co, 10)
+        f = f+1
+        print coIndex
+
+    if pm10:
+        pm10Index = index(tablePm10, pm10, 1)
+        f = f+1
+        print pm10Index
+
+    if no2:
+        no2Index = index(tableNo2, no2, 1000)
+        f = f+1
+        print no2Index
+
+    if f > 0:
+        return (coIndex+pm10Index+no2Index)/f
 
 
 @endpoints.api(name='airup', version='v1')
@@ -146,6 +212,36 @@ class AirupApi(remote.Service):
         return STORED_RECORDS
 
 
+    MULTIPLY_METHOD_RESOURCE_ZONEDETAIL = endpoints.ResourceContainer(ZoneDetail)
+    @endpoints.method(MULTIPLY_METHOD_RESOURCE_ZONEDETAIL, ZoneDetail, path='zone', http_method='POST', name='zones.saveZone')
+    def zones_saveZone(self, request):
+        print "Zonesave"
+        index = request.index
+        co = request.co
+        no2 = request.no2
+        title = request.title
+        subtitle = request.subtitle
+        max24Hr = request.max24Hr
+        min24Hr = request.min24Hr
+        timestamp = request.timestamp
+        if not timestamp:
+            timestamp = float(time.time())
+
+        taskqueue.add(queue_name='recordQueue', url='/zoneWworker', params={
+            'no2':no2,
+            'co':co,
+            'title':title,
+            'subtitle':subtitle,
+            'max24Hr':max24Hr,
+            'min24Hr':min24Hr,
+            'timestamp':timestamp,
+            'index':index
+            })
+
+        return ZoneDetail()
+
+
+
     MULTIPLY_METHOD_RESOURCE = endpoints.ResourceContainer(Record)
     @endpoints.method(MULTIPLY_METHOD_RESOURCE, Record, path='queueIt', http_method='POST', name='records.queueRecord')
     def records_queueRecord(self, request):
@@ -153,15 +249,12 @@ class AirupApi(remote.Service):
 
         timestamp = request.timestamp
         if not timestamp:
-            # now = datetime.datetime.now()
-            # timestamp = now.strftime("%Y-%m-%dT%H:%M:%S")
-            # timestamp=int(now)
             timestamp = float(time.time())
         pm10 = request.pm10
         co = request.co
         no2 = request.no2
-        ttt = datetime.datetime.now()
-        #index = request.index
+        #ttt = datetime.datetime.now()
+        index = aqi({"co":co,"pm10":pm10,"no2":2})
         #indexLabel = request.indexLabel
         position = request.position
         if not position:
@@ -197,43 +290,81 @@ class AirupApi(remote.Service):
 
 
     ID_RESOURCE2 = endpoints.ResourceContainer(message_types.VoidMessage,id=messages.StringField(1, variant=messages.Variant.STRING))
-    @endpoints.method(ID_RESOURCE2, ZoneMessage,path='report/{id}', http_method='GET',name='report.getReport')
+    @endpoints.method(ID_RESOURCE2, ZoneMessage,path='zones/{id}', http_method='GET',name='zones.getReport')
     def report_get(self, request):
         try:
-            r = db.GqlQuery("SELECT * FROM Report WHERE name='" + request.id + "'")
-            #my_dict = {'amount': '31', 'type': 'fish'}
-            #print r[0].report
+            res = db.GqlQuery("SELECT * FROM ZoneDetailPersist")
+
+            zonesArr = []
+            for r in res:
+                print r.key().id()
+
+                zonesArr.append(
+                    ZoneDetail(
+                        id=float(r.key().id()),
+                        title=r.title,
+                        subtitle=r.subtitle,
+                        index=r.index + random.randint(0,20),
+                        co=r.co + random.randint(0,9),
+                        no2=r.no2 + random.randint(0,9),
+                        min24Hr=r.min24Hr,
+                        max24Hr=r.max24Hr
+                        )
+                    )
+
             return ZoneMessage(
                 key=request.id,
                 timestamp=float(time.time()),
                 today= TodayType(best=TodayDetail(index=13.0, location="Antartica"), worst=TodayDetail(index=425.0, location="Beijing")),
-
-                zones = [
-                    ZoneDetail(id=1111.0,hood='Hornstull',hoodDescription='Stockholm, Sweden',index=34.0,co=33.0,no2=122.0,min24Hr=13.0,max24Hr=77.0,timestamp=float(time.time())),
-                    ZoneDetail(id=3333.0,hood='Chueca',hoodDescription='Madrid, Spain',index=62.0,co=44.0,no2=102.0,min24Hr=30.0,max24Hr=80.0,timestamp=float(time.time())),
-                    ZoneDetail(id=4444.0,hood='Mitte',hoodDescription='Berlin, Germany',index=40.0,co=22.0,no2=98.0,min24Hr=35.0,max24Hr=79.0,timestamp=float(time.time())),
-                    ZoneDetail(id=5555.0,hood='Harajuku',hoodDescription='Tokyo, Japan',index=54.0,co=66.0,no2=77.0,min24Hr=23.0,max24Hr=122.0,timestamp=float(time.time())),
-                    ZoneDetail(id=2222.0,hood='Park Slope',hoodDescription='New York, USA',index=101.0,co=77.0,no2=140.0,min24Hr=76.0,max24Hr=89.0,timestamp=float(time.time()))
-                    ]
+                zones = zonesArr
                 )            
         except (IndexError, TypeError):
-            raise endpoints.NotFoundException('Report %s not found.' %
-                                              (request.id,))
+            raise endpoints.NotFoundException('Report %s not found.' % (request.id,))
 
     LATLONG_RESOURCE = endpoints.ResourceContainer(message_types.VoidMessage,lat=messages.StringField(1, variant=messages.Variant.STRING),long=messages.StringField(2, variant=messages.Variant.STRING))
     @endpoints.method(LATLONG_RESOURCE, ZoneDetail, path='location/lat/{lat}/long/{long}', http_method='GET', name='report.getLocation')
     def location_get(self, request):
-        return ZoneDetail(id=1111.0,hood='Hornstull',hoodDescription='Stockholm, Sweden',index=34.0,co=33.0,no2=122.0,min24Hr=13.0,max24Hr=77.0,timestamp=float(time.time()))
+        return ZoneDetail(id=4793444555816960.0,title='Hornstull',subtitle='Stockholm, Sweden',index=200.0,co=24.0,no2=111.0,min24Hr=23.0,max24Hr=89.0,timestamp=float(time.time()))
 
 
     LOCATIONS_RESOURCE = endpoints.ResourceContainer(message_types.VoidMessage,zone=messages.FloatField(1,repeated=True))
-    @endpoints.method(LOCATIONS_RESOURCE, ZoneMessage, path='zones', http_method='GET', name='report.getZones')
+    @endpoints.method(LOCATIONS_RESOURCE, ZoneMessage, path='zones', http_method='GET', name='zones.getRequestedZones')
     def locations_get(self, request):
-        for loc in request.zone:
-            print loc
-        return ZoneMessage()
+
+        try:
+
+            qArr = []
+            for z in request.zone:
+                qArr.append("KEY('ZoneDetailPersist'," + str(int(z)) + ")")
+
+            res = db.GqlQuery("SELECT * FROM ZoneDetailPersist WHERE __key__ IN (" + ", ".join(qArr) + ")")
+            
+            zonesArr = []
+            for r in res:
+
+                zonesArr.append(
+                    ZoneDetail(
+                        id=float(r.key().id()),
+                        title=r.title,
+                        subtitle=r.subtitle,
+                        index=r.index + random.randint(0,20),
+                        co=r.co + random.randint(0,9),
+                        no2=r.no2 + random.randint(0,9),
+                        min24Hr=r.min24Hr,
+                        max24Hr=r.max24Hr
+                        )
+                    )
+
+            return ZoneMessage(
+                timestamp=float(time.time()),
+                today= TodayType(best=TodayDetail(index=13.0, location="Antartica"), worst=TodayDetail(index=425.0, location="Beijing")),
+                zones = zonesArr
+                )  
 
 
+            return ZoneMessage()
+        except (IndexError, TypeError):
+            raise endpoints.NotFoundException(IndexError)
 
 
 
