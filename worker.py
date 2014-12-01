fe22de792f187d54a0095a4969987a1a19fe43f6
Paul Lernmark <paul@lernmark.net>
2014-12-01 13:42:03 +0100

    Nu rätt URL

--- worker.py
+++ worker.py
@@ -45,8 +45,8 @@ from google.appengine.ext import db
 import hashlib
 
 GEOLOCATION_URL = "https://maps.googleapis.com/maps/api/geocode/json?language=en&key=AIzaSyA1WnmUgVJtsGuWoyHh-U8zlKRcGlSACXU&latlng=%s"
-SERVICE_URL = "http://localhost:8888"
-#SERVICE_URL = "https://bamboo-zone-547.appspot.com"
+#SERVICE_URL = "http://localhost:8888"
+SERVICE_URL = "https://bamboo-zone-547.appspot.com"
 
 JINJA_ENVIRONMENT = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),extensions=['jinja2.ext.autoescape'],autoescape=True)
 
